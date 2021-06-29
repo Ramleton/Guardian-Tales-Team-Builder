36c6ce4d84c7e8dce45707ce35c9e718b39338d2
BlakeIsMyWaifu <joel.dk@btinternet.com>
2021-06-29 09:48:56 +0100

    refactor init

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import { Character, GuardianContext } from "./Contexts/GuardianContext";
-import React, { useState } from "react";
-
-import GuardianList from "./Components/GuardianList";
-import TeamInfo from "./Components/TeamInfo";
+import React from 'react'
 
 const App: React.FC = () => {
-
-  const [builtTeam, setbuiltTeam]: [string[], CallableFunction] = useState([]);
-
-  const getGuardianData = () => {
-    const data: Character = require('./guardian_stats.json');
-    return Object.entries(data).map(([chrName, character]) => {
-      character.name = chrName;
-      character.img = `/Images/Character_Portraits/${chrName.replace("/", "")}.jpg`;
-      return character;
-    });
-  }
-
-  return (
-      <div className="dark-background container">
-        <GuardianContext.Provider value={getGuardianData()}>
-          <GuardianList buildTeam={setbuiltTeam}/>
-          <TeamInfo teamMembers={builtTeam}/>
-        </GuardianContext.Provider>
-      </div>
-  )
+	return (
+		<div>
+			<p>Init</p>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/Contexts/TeamContext.ts
+++
@@ -1,6 +0,0 @@
-import { Character } from "./GuardianContext";
-import { createContext } from "react";
-
-const defaultState: Character[] = [];
-
-export const TeamContext = createContext(defaultState);
